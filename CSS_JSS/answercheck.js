5b0f8a2417f2d05d5b1f509ee21070182e7ec259
LearnPythonKSZ <33370042+LearnPythonKSZ@users.noreply.github.com>
2017-12-03 11:01:15 +0100

    Add files via upload

---
+++ CSS_JSS/answercheck.js
@@ -0,0 +1,7 @@
+function answerfunction() {
+    var answer=document.getElementById("answercheck").value;
+	if (answer=="false"||answer=="False"){
+		document.getElementById("next").className="nextunlocked";
+		document.getElementById("next").value="Nächstes Kapitel";
+	}
+}
