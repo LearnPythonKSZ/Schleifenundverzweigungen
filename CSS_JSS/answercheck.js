9b8514dfea9e8f870da4404574f7fbb2a8f12aea
LearnPythonKSZ <33370042+LearnPythonKSZ@users.noreply.github.com>
2017-12-10 12:20:48 +0100

    Update answercheck.js

--- CSS_JSS/answercheck.js
+++ CSS_JSS/answercheck.js
@@ -2,7 +2,7 @@ function answerfunction() {
     var answer=document.getElementById("answercheck").value;
 	if (answer=="false"||answer=="False"){
 		document.getElementById("next").className="nextunlocked";
-		document.getElementById("next").value="Nächstes Kapitel";
-		document.getElementById("buttonhref").href="/Operatoren";
+		document.getElementById("next").value="Lernkurs beendet, zur AboutMe Seite";
+		document.getElementById("buttonhref").href="/AboutMe";
 	}
 }
