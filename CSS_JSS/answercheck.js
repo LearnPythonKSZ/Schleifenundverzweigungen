e092e8782598988d699a60dbb8d5c87a0c042099
LearnPythonKSZ <33370042+LearnPythonKSZ@users.noreply.github.com>
2017-12-12 21:27:54 +0100

    Delete answercheck.js

--- CSS_JSS/answercheck.js
+++
@@ -1,8 +0,0 @@
-function answerfunction() {
-    var answer=document.getElementById("answercheck").value;
-	if (answer=="false"||answer=="False"){
-		document.getElementById("next").className="nextunlocked";
-		document.getElementById("next").value="Lernkurs beendet, zur AboutMe Seite";
-		document.getElementById("buttonhref").href="/AboutMe";
-	}
-}
