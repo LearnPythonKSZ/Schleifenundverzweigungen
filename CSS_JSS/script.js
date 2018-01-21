811e4b6babdfa17630029315db85214a72a3258b
LearnPythonKSZ <33370042+LearnPythonKSZ@users.noreply.github.com>
2018-01-21 16:36:03 +0100

    Update script.js

--- CSS_JSS/script.js
+++ CSS_JSS/script.js
@@ -14,6 +14,40 @@ function number(){
 		document.getElementById("unlocked").innerHTML="Du hast das nächste Kapitel freigeschaltet";
 	}
 }
+function solution(idbutton){
+ 	if (idbutton.getAttribute("id")=="solutionbutton1"){
+ 		var solutiontag=document.getElementById("solutiontag1");
+ 		var solutionbutton=document.getElementById("solutionbutton1");
+ 		var solutionspan=document.getElementById("solution1");
+ 		solutionbutton.style.width = "800px";
+ 		solutionbutton.style.height = "100px";
+ 		solutionbutton.style.left="calc(50% - 400px)";
+ 		solutiontag.style.display="none";
+ 		solutionspan.style.display="inline";
+ 	}else if(idbutton.getAttribute("id")=="solutionbutton2"){
+ 		var solutiontag=document.getElementById("solutiontag2");
+ 		var solutionbutton=document.getElementById("solutionbutton2");
+ 		var solutionspan=document.getElementById("solution2");
+ 		solutionbutton.style.width = "800px";
+ 		solutionbutton.style.height = "100px";
+ 		solutionbutton.style.left="calc(50% - 400px)";
+ 		solutiontag.style.display="none";
+ 		solutionspan.style.display="inline";
+ 	}
+ }
+ function help(idtext,idbutton,scroll){
+	var idh = idbutton.getAttribute("id");
+ 	if (idh == "helpbutton1") {
+ 	idbutton.style.display = "none";
+ 	idtext.style.display = "block";
+ 	} else if (idh=="helpbutton2") {
+ 	idbutton.style.display = "none";
+	location.href = "#scrollto";
+ 	idtext.style.backgroundColor="yellow";
+	}else{ 
+     alert("an error occured pls contact system admin #1");
+ 	}
+ }
 function pointunlock() {
 	points = pointgap + pointanswerfunction + pointquestion;
 	differenz= pointsneeded-points;
